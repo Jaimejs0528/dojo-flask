13adbc5b4f44f2f73269ec4b20031eccaee7d56f
Jaime Jimenez Serrano <jaime.jimenezs@udea.edu.co>
2019-07-27 09:54:32 -0500

    creating server

--- cars-server/api/factory.py
+++ cars-server/api/factory.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from flask_restful import Api
+
+from api.controller.car import Car
 
 app = Flask(__name__)
 
-app.
+# Configure CORS
+cors = CORS(app,  origins=['http://localhost:8080'])
+
+app.config['MONGO_URI'] = 'mongodb://3.14.7.212:27017/Cars'
+app.database = PyMongo(app)
+app.cards_collection = app.database.db.cars
+
+# Create API
+api = Api(app)
+
+api.add_resource(Car, '/car', '/car/<car_id>',
+                 methods=['GET', 'POST', 'PUT', 'DELETE'])
